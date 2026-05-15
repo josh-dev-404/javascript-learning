8494317ab1d5878ce4e7691092a7b3dbc3a49fa8
Josh-devx-404 <joshdevx40@gmail.com>
2026-05-15 17:43:52 +0800

    JavaScript Course Completed

---
+++ 01. Beginners/02. Primitives/02 Numbers/index.js
@@ -0,0 +1,19 @@
+// 1. Create Variable name (firstFavNum) & store your favorite number.
+// 2. Create Variable name (secondFavNum) & store your second fav number.
+// 3. Add (firstFavNum & secondFavNumber).
+// 4. Subtract (firstFavNum & secondFavNumber).
+// 5. Multiply (firstFavNum & secondFavNumber).
+// 6. Divided (firstFavNum & secondFavNumber).
+// 7. Check (firstFavNum Mod secondFavNumber)
+// 8. Check the power of (firstFavNum)
+
+let num1 = 7;
+let num2 = 3;
+let favNmber = 5;
+
+console.log(num1 += num2);
+console.log(num1 -= num2);
+console.log(num1 *= num2);
+console.log(num1 /= num2);
+console.log(num1 %= num2);
+console.log(favNmber **= 2);
