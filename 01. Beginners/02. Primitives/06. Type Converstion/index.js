e7f94ed12089a2b28843cc5285fc599f44736ee2
Josh-devx-404 <joshdevx40@gmail.com>
2026-05-19 22:43:14 +0800

    Convert string to integer and float

---
+++ 01. Beginners/02. Primitives/06. Type Converstion/index.js
@@ -0,0 +1,12 @@
+// - Convert string to integer
+// - Convert string to float
+
+
+let balance = "2000.50";
+let total = "500.25";
+// convert to integer using parseInt
+let pay = parseInt(balance - total);
+console.log(pay);
+// convert to float using parseFloat
+pay = parseFloat(balance - total);
+console.log(pay);
